0b00b72901b4854df9a540b086d8d473edf53b2f
scasano1 <scasano1@tucsonaz.gov>
2016-04-28 12:21:15 -0700

    GIS metadata models

---
+++ Base.php
@@ -0,0 +1,7 @@
+<?php
+class Base extends AppModel {
+	public $useTable = 'GIS_METADATA_BASE';
+	public $primaryKey = 'FCName';
+
+}
+?>
---
+++ GisMetadataAttributes.php
@@ -0,0 +1,11 @@
+<?php
+class GisMetadataAttributes extends AppModel {
+	public $useTable = 'GIS_METADATA_ATTRIBUTES';
+	#public $useTable = 'tpd_crime_stat';
+	#public $primaryKey = 'FCName';
+
+	#public $belongsTo = array(
+#		'Codes' => array('foreignKey' => 'crime_ucr_cd')
+#	);
+}
+?>
